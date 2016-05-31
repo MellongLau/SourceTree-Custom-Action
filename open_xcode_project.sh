11dcec784bc418b7ed0f7deb25c4e2096b5d70f5
Mellong <121382647@qq.com>
2016-05-31 22:37:20 +0800

    Fixed find command produce newline if exist space char.

--- open_xcode_project.sh
+++ open_xcode_project.sh
@@ -5,14 +5,15 @@ OPEN_TYPE=$2
 
 #判断打开项目文件的类型，根据类型筛选出项目文件路径
 if [ $OPEN_TYPE = "xcodeproj" ]; then
-    LIST=`find $REPO_PATH -name "*.xcodeproj" | grep -v "Pods.xcodeproj"`
+    LIST=`find "$REPO_PATH" -name "*.xcodeproj" | grep -v "Pods.xcodeproj"`
 else
-    LIST=`find $REPO_PATH -name "*.xcworkspace" | grep -v ".xcodeproj/project.xcworkspace"`
+    LIST=`find "$REPO_PATH" -name "*.xcworkspace" | grep -v ".xcodeproj/project.xcworkspace"`
 fi
 
-for ITEM in $LIST
+
+echo "$LIST" | while read i
 do
-#打开项目文件
-open $ITEM
+
+open "$i"
 
 done
